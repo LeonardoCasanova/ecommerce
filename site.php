c2092e8dcd4e7c202c8d17a895e73fac1e2b0ad8
Leonardo Casanova <leonardocsanova@gmail.com>
2019-03-12 22:28:58 -0300

    Carrinho de Compras

--- site.php
+++ site.php
@@ -3,6 +3,8 @@
 use \Hcode\Page;
 use Hcode\Model\Product;
 use Hcode\Model\Category;
+use Hcode\Model\Cart;
+
 
 $app->get('/', function () {
 
@@ -60,3 +62,12 @@ $app->get("/products/:desurl", function($desurl){
   
 });
 
+$app->get("/cart", function(){
+
+  $cart = Cart::getFromSession();
+
+  $page = new Page();
+  
+  $page->setTpl("cart");
+  
+});
