1e17f42379980ddfce77d2d8676cdeb58357a712
Leonardo <leonardocsanova@gmail.com>
2019-03-18 14:08:59 -0300

    Admin - User - Search and Pagination

--- admin-users.php
+++ admin-users.php
@@ -21,11 +21,40 @@ $app->get('/admin/users', function () {
 
   User::verifyLogin();
 
-  $users = User::listAll();
+  $search = (isset($_GET['search']) ? $_GET['search'] : "");
+  $page = (isset($_GET['page'])) ? (INT)$_GET['page'] : 1;
+
+
+  if($search != '') {
+
+    $pagination = User::getPageSearch($search, $page);
+  } else {
+
+    $pagination = User::getPage($page);
+  }
+
+  
+
+  $pages = [];
+
+  for ($i=0; $i < $pagination['pages']; $i++) { 
+   
+    array_push($pages,[
+      'href'=>'/admin/users?'.http_build_query([
+      'page'=>$i+1,
+      'search'=>$search
+    ]),
+    'text'=>$i+1
+    ]);
+  }
 
   $page = new PageAdmin();
 
-  $page->setTpl("users", array("users" => $users));
+  $page->setTpl("users", 
+  array("users" => $pagination['data'],
+        "search" => $search,
+        "pages" =>$pages
+ ));
 });
 
 $app->get('/admin/users/create', function () {
